48fba4c855eca369246b42ac762b71111c547c99
Soumendu Das <soumendudas@gmail.com>
2015-12-21 18:09:48 +0530

    Update JSONParser.java

--- src/com/install/app/JSONParser.java
+++ src/com/install/app/JSONParser.java
@@ -15,41 +15,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import android.util.Log;
 
 public class JSONParser {
-	private InputStream inputStreamObj = null;
-	private String jsonStr = "";
 
-	
-	public String getResponseString(String url) {
-		try {
-			DefaultHttpClient httpClient = new DefaultHttpClient();
-			HttpGet httpGet = new HttpGet(url);
-
-			HttpResponse httpResponse = httpClient.execute(httpGet);
-			HttpEntity httpEntity = httpResponse.getEntity();
-			inputStreamObj = httpEntity.getContent();           
-
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					inputStreamObj, "iso-8859-1"), 8);
-			StringBuilder sb = new StringBuilder();
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-
-			jsonStr = sb.toString();
-			inputStreamObj.close();
-		}catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			Log.e("Buffer Error", "Error converting result " + e.toString());
-		}
-		return jsonStr;
-
-	}
 
 
 }
