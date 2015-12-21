9d74aab3e78f24071c601bb8da3c4ad1b87d27b5
Soumendu Das <soumendudas@gmail.com>
2015-12-21 18:04:39 +0530

    Update PingController.java

--- src/com/install/app/PingController.java
+++ src/com/install/app/PingController.java
@@ -5,27 +5,6 @@ import android.content.Intent;
 
 public class PingController {
 	
-	private static final PingController ping = new PingController();
-	private static Context context;
 	
-	public static PingController instance(Context ctx){
-		if(context==null)
-			context=ctx;
-		return ping;
-	}
-	
-	public void startService(boolean flag, String nameStr, String subStr, int pingTime){
-		Intent intent = new Intent(context, InstallService.class);
-		intent.putExtra("foreground", flag);
-		intent.putExtra("notiname", nameStr);
-		intent.putExtra("notisub", subStr);
-		intent.putExtra("pingtime", pingTime);
-		context.startService(intent);
-	}
-
-	public void stopService(){
-		Intent intent = new Intent(context, InstallService.class);
-		context.stopService(intent);
-	}
 	
 }
